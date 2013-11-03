247a2685ecfa39067ff8477656f23e4f0cbadb1d
Elan Ruusamäe <glen@delfi.ee>
2013-11-03 19:18:41 +0200

    update metadata about default recipe

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ description      "Installs and configures gitolite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-recipe "gitolite",        "This recipe is a no-op and does nothing."
+recipe "gitolite",        "This recipe installs Gitolite dependencies perl and git "
 recipe "gitolite::basic", "Sets up the gitolite basic installation."
 
 %w( ubuntu debian ).each do |os|
